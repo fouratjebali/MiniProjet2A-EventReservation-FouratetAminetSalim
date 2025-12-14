b1b9efe911828d4340474688358583de80546e3d
Fourat Jebali <jebalifourat@issatso.u-sousse.tn>
2025-12-14 12:23:27 +0100

    Initial commit: Project structure with Docker configuration

--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../config/routes.php';
-
-$uri = $_SERVER['REQUEST_URI'];
-$uri = str_replace('/index.php', '', $uri);
-$uri = parse_url($uri, PHP_URL_PATH);
-
-route($uri);
-?>
